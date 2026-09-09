a325196037456491f426e1d0d7288313ef52b3f2
GodMaxim <max.zabashta.c4@gmail.com>
2026-09-09 20:22:31 +0300

    Load changes

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -6,15 +6,16 @@ bookingScenarios.forEach((scenario) => {
 
     test(qase(scenario.qaseId, `Booking test scenario #${scenario.id}`), async ({ page, bookingPage, contactUsPage }) => {
         await bookingPage.mockSlotBooking();
-        await page.goto('https://valtive.io/contact-valtive/');
+        await page.goto('https://valtive.io/contact-valtive/')
+        await page.waitForLoadState('networkidle').catch(() => {});
        
         await contactUsPage.selectAvailableDay()
         await expect(contactUsPage.nextBtn).toBeEnabled()
         await contactUsPage.clickNextBtn()
         await contactUsPage.waitForForm()
-        await contactUsPage.fillBookingForm(scenario.name, scenario.email);
+        await contactUsPage.fillBookingForm(scenario.name, scenario.email)
 
-        await bookingPage.forceSuccessScreen();
+        await bookingPage.forceSuccessScreen()
 
         await expect(contactUsPage.scheduledTitle).toBeVisible({ timeout: 5000 });
         await expect(contactUsPage.invitationMessage).toBeVisible({ timeout: 5000 });
