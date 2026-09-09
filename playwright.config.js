f477a67c8afec8ba472ed6f126abe49dba936dca
GodMaxim <max.zabashta.c4@gmail.com>
2026-09-09 19:17:39 +0300

    Small changes

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   timeout: 120000,
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : 1,
