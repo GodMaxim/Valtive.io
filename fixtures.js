797d463a6ebec407b8ae25f8450b546024e22626
GodMaxim <max.zabashta.c4@gmail.com>
2026-09-10 07:07:46 +0300

    Fixtures change

--- fixtures.js
+++ fixtures.js
@@ -1,4 +1,4 @@
-import { test as base } from '@playwright/test'
+import { test as base, chromium } from '@playwright/test'
 import { HomePage } from './page/HomePage.js'
 import { ContactUsPage } from './page/ContactUsPage.js'
 import { BookingPage } from './page/BookingPage.js'
