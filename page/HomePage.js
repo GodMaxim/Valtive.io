5fcff420de37df5f173e925a2abec761d89efd3a
GodMaxim <max.zabashta.c4@gmail.com>
2026-09-10 09:30:32 +0300

    New test run

--- page/HomePage.js
+++ page/HomePage.js
@@ -3,22 +3,10 @@ export class HomePage {
     constructor(page) {
         this.page = page
         this.contactUsButton = page.locator('#menu-main-menu a[href="https://valtive.io/contact-valtive/"]')
+        this.title = page.getByRole('heading', { name: '+167 years of combined' }).first();
     }
 
     async goToContactUsPage() {
-        await this.page.route('**/*', (route) => {
-            const url = route.request().url();
-            if (url.includes('google-analytics') || url.includes('hotjar') || url.includes('facebook')) {
-                return route.abort();
-            }
-            return route.continue();
-        });
-
-        await this.page.waitForTimeout(3000);
-        await this.page.goto('https://valtive.io/contact-valtive/', { 
-            waitUntil: 'domcontentloaded',
-            timeout: 45000 
-        });
-        await this.contactUsButton.waitFor({ state: 'visible', timeout: 15000 })
+        await this.contactUsButton.click()
     }
 }
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,12 +1,28 @@
 import { test, expect } from '../fixtures.js'
 import { qase } from 'playwright-qase-reporter'
 import { bookingScenarios } from '../utils/bookingData.js'
-import { HomePage } from '../page/HomePage.js'
+
 
 bookingScenarios.forEach((scenario) => {
 
     test(qase(scenario.qaseId, `Booking test scenario #${scenario.id}`), async ({ page, bookingPage, contactUsPage, homePage }) => {
         await page.waitForTimeout(4000)
+
+        await page.route('**/*', (route) => {
+            const url = route.request().url();
+            if (url.includes('google-analytics') || url.includes('hotjar') || url.includes('facebook')) {
+                return route.abort();
+            }
+            return route.continue();
+        });
+
+        await page.goto('https://valtive.io/', {
+            waitUntil: 'domcontentloaded', 
+            timeout: 45000
+        })
+
+        await expect(homePage.title).toBeVisible()
+
         await homePage.goToContactUsPage()
 
         const iframeElement = page.locator('iframe[src*="calendly.com"]');
